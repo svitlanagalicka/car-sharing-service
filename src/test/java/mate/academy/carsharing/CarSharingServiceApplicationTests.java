7af89818aa4e2f47de1fc2bc96bdafd4e03973bb
Svitlana Galicka <svitlanakozak12332@gmail.com>
2025-08-02 17:24:41 +0300

    first commit

---
+++ src/test/java/mate/academy/carsharing/CarSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.carsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
