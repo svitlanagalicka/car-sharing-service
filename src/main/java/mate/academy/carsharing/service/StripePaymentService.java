88c99f4f916e821e12800cc342121a395a114390
Svitlana Galicka <svitlanakozak12332@gmail.com>
2025-09-02 22:10:55 +0300

    added StripePaymentService

--- src/main/java/mate/academy/carsharing/service/StripePaymentService.java
+++ src/main/java/mate/academy/carsharing/service/StripePaymentService.java
@@ -1,10 +1,9 @@
 package mate.academy.carsharing.service;
 
 import com.stripe.model.checkout.Session;
+import java.math.BigDecimal;
 import mate.academy.carsharing.model.Payment;
 import mate.academy.carsharing.model.Rental;
-
-import java.math.BigDecimal;
 
 public interface StripePaymentService {
     Session createStripeSession(Payment payment, Rental rental, BigDecimal amount);
--- src/main/java/mate/academy/carsharing/service/StripePaymentServiceImpl.java
+++ src/main/java/mate/academy/carsharing/service/StripePaymentServiceImpl.java
@@ -4,9 +4,8 @@ import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
-import java.math.BigDecimal;
-
 import jakarta.annotation.PostConstruct;
+import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import mate.academy.carsharing.model.Payment;
 import mate.academy.carsharing.model.Rental;
